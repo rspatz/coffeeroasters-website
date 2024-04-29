51087ce0277b0123d19ac0d4b833c1951c5574d1
Robert <rspatz@live.com>
2024-04-29 08:12:02 -0500

    Build mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById('top-container');
-  if (x.className === 'top-container') {
-    x.className += ' responsive';
+// function myFunction() {
+//   var x = document.getElementById('top-container');
+//   if (x.className === 'top-container') {
+//     x.className += ' responsive';
+//   } else {
+//     x.className = 'top-container';
+//   }
+// }
+
+const mainNav = document.querySelector('.main-nav-list');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  const visibility = mainNav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    mainNav.setAttribute('data-visible', true);
+    navToggle.setAttribute('aria-expanded', true);
   } else {
-    x.className = 'top-container';
+    mainNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
   }
-}
+});
+
+// https://www.youtube.com/watch?v=HbBMp6yUXO0&t=2509s
+// Begin @ 38'' 0'''
